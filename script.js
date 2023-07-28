5ff432c1025dc79f849ef41eb273fe8d873cf497
datawithjulianuribe <134468958+datawithjulianuribe@users.noreply.github.com>
2023-07-27 22:41:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //CAMBIO DE IMAGENES EN LA PAGINA PRENSA.HTML
 
 var image = document.getElementById("dynamic-image");
-var images = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg", "10.jpeg"]; // Rutas de las imágenes
+var images = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "12.jpeg", "9.jpeg", "10.jpeg"]; // Rutas de las imágenes
 
 var index = 0;
 
